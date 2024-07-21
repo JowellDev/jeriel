e211598e3be3e1ebfbd49b6f61ef0c277988c536
Joël Ephraïm Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2024-07-21 16:31:13 +0000

    feat: update input-field styles

--- app/routes/_auth+/login+/components/login-form.tsx
+++ app/routes/_auth+/login+/components/login-form.tsx
@@ -9,6 +9,8 @@ import PasswordInputField from '~/components/form/password-input-field'
 import { useRedirectTo } from '../hooks/redirect'
 import { schema, type ActionType } from '../action.servser'
 import { PasswordForgottenLink } from './password-forgotten-link'
+import { Alert, AlertDescription } from '~/components/ui/alert'
+import { RiInformationLine } from '@remixicon/react'
 
 export function LoginForm() {
 	const redirectToFromQuery = useRedirectTo()
@@ -34,18 +36,30 @@ export function LoginForm() {
 			method="post"
 			action="."
 		>
+			{form.errors?.length && (
+				<Alert variant="destructive">
+					<AlertDescription className="flex space-x-2">
+						<RiInformationLine size={20} />
+						<span>Email et/ou mot de passe invalide(s)</span>
+					</AlertDescription>
+				</Alert>
+			)}
 			<InputField
 				field={email}
 				label="Email"
-				InputProps={{ autoComplete: 'email' }}
+				InputProps={{ autoComplete: 'email', className: 'bg-white' }}
 			/>
 			<div>
-				<PasswordInputField label="Mot de passe" field={password} />
+				<PasswordInputField
+					label="Mot de passe"
+					field={password}
+					InputProps={{ className: 'bg-white' }}
+				/>
 				<PasswordForgottenLink />
 			</div>
 			<input
 				{...getInputProps(redirectTo, {
-					type: 'text',
+					type: 'hidden',
 					hidden: true,
 					ariaAttributes: false,
 				})}
