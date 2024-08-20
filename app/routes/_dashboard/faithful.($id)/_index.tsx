982d3c8cf7f561bcf99e9463c760e741e7b968e1
Joël Ephraïm Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2024-08-20 17:48:45 +0000

    feat: update fairthful main page

--- app/routes/_dashboard/faithful.($id)/_index.tsx
+++ app/routes/_dashboard/faithful.($id)/_index.tsx
@@ -13,11 +13,17 @@ import { useDebounceCallback } from 'usehooks-ts'
 import SpeedDialMenu, {
 	type SpeedDialAction,
 } from '~/components/layout/mobile/speed-dial-menu'
-import { RiAddLine } from '@remixicon/react'
+import { RiAddLine, RiArrowDownSLine } from '@remixicon/react'
 import { FairthfulTable } from './components/fairthful-table'
 import { loaderFn } from './loader.server'
 import type { FairthfulWithMonthlyAttendances } from './types'
 import { Card } from '~/components/ui/card'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu'
 
 export const meta: MetaFunction = () => [{ title: 'Gestion des fidèles' }]
 
@@ -30,7 +36,7 @@ export const loader = loaderFn
 export default function Faithful() {
 	const { data } = useLoaderData<typeof loaderFn>()
 	const fetcher = useFetcher()
-	const [openForm, setOpenForm] = useState(false)
+	const [openManualForm, setOpenManualForm] = useState(false)
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [searchParams, setSearchParams] = useSearchParams()
@@ -41,7 +47,7 @@ export default function Faithful() {
 	}
 
 	const handleSpeedDialItemClick = (action: string) => {
-		if (action === 'add-faithful') setOpenForm(true)
+		if (action === 'add-faithful') setOpenManualForm(true)
 	}
 
 	return (
@@ -53,13 +59,25 @@ export default function Faithful() {
 							<InputSearch onSearch={handleSearch} placeholder="Recherche..." />
 						</fetcher.Form>
 					</div>
-					<Button
-						className="hidden sm:block"
-						variant={'gold'}
-						onClick={() => setOpenForm(true)}
-					>
-						Ajouter un fidèle
-					</Button>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button className="hidden sm:flex items-center" variant={'gold'}>
+								<span>Ajouter un fidèle</span>
+								<RiArrowDownSLine size={20} />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent className="mr-3 ">
+							<DropdownMenuItem
+								className="cursor-pointer"
+								onClick={() => setOpenManualForm(true)}
+							>
+								Ajouter manuellement
+							</DropdownMenuItem>
+							<DropdownMenuItem className="cursor-pointer">
+								Importer un fichier
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
 				</Header>
 			}
 		>
@@ -83,7 +101,7 @@ export default function Faithful() {
 					</div>
 				</Card>
 			</div>
-			{openForm && <div>form here</div>}
+			{openManualForm && <div>form here</div>}
 			<SpeedDialMenu
 				items={speedDialItems}
 				onClick={handleSpeedDialItemClick}
