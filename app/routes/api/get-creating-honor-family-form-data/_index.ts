a0fb4c29cd68f77c07d24562b64cb68d4dd845d1
SoroMoise <soromoise4@gmail.com>
2024-08-28 23:39:04 +0000

    feat(api): get only unused member

--- app/routes/api/get-creating-honor-family-form-data/_index.ts
+++ app/routes/api/get-creating-honor-family-form-data/_index.ts
@@ -1,22 +1,37 @@
-import { json } from '@remix-run/node'
+import { Role } from '@prisma/client'
+import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { requireUser } from '~/utils/auth.server'
 import { prisma } from '~/utils/db.server'
 
-export const loader = async () => {
-	const users = (
-		await prisma.user.findMany({
-			where: { isActive: true, honorFamilyId: null },
-			select: { id: true, name: true, phone: true },
-			orderBy: { name: 'asc' },
-		})
-	).map(({ id, name, phone }) => ({ label: name ?? phone, value: id }))
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const currentUser = await requireUser(request)
 
 	const admins = (
 		await prisma.user.findMany({
-			where: { isActive: true, honorFamilyAdmin: null },
-			select: { id: true, name: true, phone: true, honorFamilyAdmin: true },
+			where: {
+				isActive: true,
+				honorFamilyId: null,
+				churchId: currentUser.churchId,
+				isAdmin: { not: true },
+			},
+			select: { id: true, name: true, honorFamilyManager: true },
 			orderBy: { name: 'asc' },
 		})
-	).map(({ id, name, phone }) => ({ label: name ?? phone, value: id }))
+	).map(({ id, name }) => ({ label: name, value: id }))
+
+	const users = (
+		await prisma.user.findMany({
+			where: {
+				isActive: true,
+				honorFamilyId: null,
+				churchId: currentUser.churchId,
+				honorFamilyManager: { is: null },
+				isAdmin: { not: true },
+			},
+			select: { id: true, name: true },
+			orderBy: { name: 'asc' },
+		})
+	).map(({ id, name }) => ({ label: name, value: id }))
 
 	return json({ users, admins })
 }
