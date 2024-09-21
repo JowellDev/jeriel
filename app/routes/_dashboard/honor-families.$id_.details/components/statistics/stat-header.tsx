496fd0b0905ed22ec9abad7c78ab4269e04d88ac
SoroMoise <soromoise4@gmail.com>
2024-09-21 19:32:36 +0000

    feat: add statistics components

---
+++ app/routes/_dashboard/honor-families.$id_.details/components/statistics/stat-header.tsx
@@ -0,0 +1,36 @@
+import { type PropsWithChildren } from 'react'
+import { ViewButtons } from '../views-buttons'
+import { type ViewOption } from '../../types'
+
+type Props = PropsWithChildren<{
+	title: string
+	view: ViewOption
+	setView: (view: ViewOption) => void
+}>
+
+export function StatHeader({
+	title,
+	children,
+	view,
+	setView,
+}: Readonly<Props>) {
+	return (
+		<div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4 sm:p-4 p-8 bg-white shadow">
+			<div className="text-sm flex items-center space-x-2">
+				<h1 className="text-lg sm:text-xl font-bold mb-2 sm:mb-0 mt-[3.5rem] sm:mt-0 ml-6 sm:ml-0 text-[#226C67]">
+					{title}
+				</h1>
+
+				<ViewButtons
+					activeView={view}
+					setView={setView}
+					excludeOptions={['stat']}
+				/>
+			</div>
+
+			<div className="flex flex-col gap-2 sm:gap-0 sm:flex-row sm:items-center sm:space-x-2">
+				{children}
+			</div>
+		</div>
+	)
+}
---
+++ app/routes/_dashboard/honor-families.$id_.details/components/statistics/stat-table.tsx
@@ -0,0 +1,92 @@
+import { RiExternalLinkLine } from '@remixicon/react'
+import {
+	useReactTable,
+	getCoreRowModel,
+	flexRender,
+} from '@tanstack/react-table'
+import {
+	Table,
+	TableHeader,
+	TableRow,
+	TableHead,
+	TableBody,
+	TableCell,
+} from '~/components/ui/table'
+import { getColumns } from './stat-colums'
+import { Button } from '~/components/ui/button'
+import { getMonthSundays } from '~/utils/date'
+import { sub } from 'date-fns'
+import type { MemberMonthlyAttendances } from '~/models/member.model'
+import { Link } from '@remix-run/react'
+
+interface Props {
+	data: MemberMonthlyAttendances[]
+	tribeId: string
+}
+export function StatTable({ data, tribeId }: Readonly<Props>) {
+	const lastMonth = sub(new Date(), { months: 1 })
+	const currentMonthSundays = getMonthSundays(new Date())
+	const table = useReactTable({
+		data,
+		columns: getColumns(currentMonthSundays, lastMonth),
+		getCoreRowModel: getCoreRowModel(),
+	})
+
+	return (
+		<Table>
+			<TableHeader>
+				{table.getHeaderGroups().map(headerGroup => (
+					<TableRow key={headerGroup.id}>
+						{headerGroup.headers.map(header => (
+							<TableHead key={header.id} className="font-semibold">
+								{header.isPlaceholder
+									? null
+									: flexRender(
+											header.column.columnDef.header,
+											header.getContext(),
+										)}
+							</TableHead>
+						))}
+					</TableRow>
+				))}
+			</TableHeader>
+			<TableBody>
+				{table.getRowModel().rows?.length ? (
+					table.getRowModel().rows.map(row => (
+						<TableRow
+							key={row.id}
+							data-state={row.getIsSelected() && 'selected'}
+						>
+							{row.getVisibleCells().map(cell => {
+								return cell.column.id === 'actions' ? (
+									<TableCell key={cell.id}>
+										<Link
+											to={`/members/${row.original.id}/details?from=tribe&id=${tribeId}`}
+										>
+											<Button variant="ghost" size="icon-sm">
+												<RiExternalLinkLine size={20} />
+											</Button>
+										</Link>
+									</TableCell>
+								) : (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								)
+							})}
+						</TableRow>
+					))
+				) : (
+					<TableRow>
+						<TableCell
+							colSpan={getColumns(currentMonthSundays, lastMonth).length}
+							className="h-24 text-center"
+						>
+							Aucune donnée.
+						</TableCell>
+					</TableRow>
+				)}
+			</TableBody>
+		</Table>
+	)
+}
