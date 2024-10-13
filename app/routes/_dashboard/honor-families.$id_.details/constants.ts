15e96b9ea51f27dd3b7bb610953e44afbf4e747f
SoroMoise <soromoise4@gmail.com>
2024-10-13 03:33:22 +0000

    chore: auto fix

--- app/routes/_dashboard/honor-families.$id_.details/constants.ts
+++ app/routes/_dashboard/honor-families.$id_.details/constants.ts
@@ -1,6 +1,6 @@
 import { RiAddLine, RiFileExcel2Line } from '@remixicon/react'
 import type { SpeedDialAction } from '~/components/layout/mobile/speed-dial-menu'
-import { ViewOption, VIEWS } from './types'
+import { VIEWS, type ViewOption } from './types'
 
 export const stateFilterData = [
 	{ value: 'ALL', label: 'Tout' },
