a19b851b5b4a9cd605cf3ffd2b29608e3917d567
Joël Ephraïm Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2025-01-18 22:50:51 +0000

    feat: update church's module tables

--- app/routes/_dashboard/churches.($id)/components/columns.tsx
+++ app/routes/_dashboard/churches.($id)/components/columns.tsx
@@ -3,16 +3,41 @@ import type { Church } from '../model'
 
 export const columns: ColumnDef<Church>[] = [
 	{
-		accessorKey: 'name',
 		header: 'Eglise',
+		cell: ({ row }) => {
+			const { name } = row.original
+
+			return (
+				<div className="flex space-x-4 items-center text-[11px] sm:text-sm">
+					{name}
+				</div>
+			)
+		},
 	},
 	{
-		accessorKey: 'admin.name',
 		header: 'Admin',
+		cell: ({ row }) => {
+			const { admin } = row.original
+
+			return (
+				<div className="flex space-x-4 items-center text-[11px] sm:text-sm">
+					{admin.name}
+				</div>
+			)
+		},
 	},
 	{
 		accessorKey: 'admin.phone',
 		header: 'Téléphone',
+		cell: ({ row }) => {
+			const { admin } = row.original
+
+			return (
+				<div className="flex space-x-4 items-center text-[11px] sm:text-sm">
+					{admin.phone}
+				</div>
+			)
+		},
 	},
 	{
 		id: 'actions',
