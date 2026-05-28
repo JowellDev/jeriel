1a01bdab82493a6b39eafdd966fe2dac633c77a3
Joël Ephraïm Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2026-05-28 15:01:00 +0000

    chore(dshboard): remove unused utils

--- app/routes/_dashboard/dashboard/utils/generate-data.ts
+++ app/routes/_dashboard/dashboard/utils/generate-data.ts
@@ -1,40 +1,4 @@
-import type {
-	AttendanceAdminStats,
-	EntityWithStats,
-	PieChartData,
-} from '../types'
-
-export function generatePieChartData(
-	newMembers: number,
-	oldMembers: number,
-): PieChartData {
-	const total = newMembers + oldMembers
-
-	return {
-		data: [
-			{ member: 'nouveaux', members: newMembers, fill: '#3BC9BF' },
-			{ member: 'anciens', members: oldMembers, fill: '#F68D2B' },
-		],
-		config: {
-			nouveaux: {
-				label: 'Nouveaux',
-				color: '#3BC9BF',
-				value:
-					total === 0
-						? 0
-						: Math.round((newMembers / (newMembers + oldMembers)) * 100),
-			},
-			anciens: {
-				label: 'Anciens',
-				color: '#F68D2B',
-				value:
-					total === 0
-						? 0
-						: Math.round((oldMembers / (newMembers + oldMembers)) * 100),
-			},
-		},
-	}
-}
+import type { AttendanceAdminStats, EntityWithStats } from '../types'
 
 export function generateLineChartData(stats: AttendanceAdminStats[]) {
 	return {
